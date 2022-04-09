5c59335040243e9ca9109f5ee2242268fa85c53b
Marcus L <marcu99lee@gmail.com>
2022-04-09 00:08:28 -0400

    css finalized, moved on the js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,53 @@
+let appRating = () => {
+    let selection = 0;
 
+    let btnOne = document.querySelector('#one')
+    console.log('btnOne')
 
+    let ratingSelection = () => {
+        let youSelected = document.querySelector('.you-selected')
+        let btnOne = document.querySelector('.rating .one');
+        console.log('btnOne')
+        let btnTwo = document.querySelector('#two');
+        let btnThree = document.querySelector('#three');
+        let btnFour = document.querySelector('#four');
+        let btnFive = document.querySelector('#five');
+        let ratingOption = [btnOne, btnTwo, btnThree, btnFour, btnFive]
 
+        ratingOption.forEach(option => {
+            option.addEventListener("click", function () {
+                youSelected.textContent = `You selected ${selection} out of 5.`;
+          })
+        });
 
+        // switch (selection) {
+        //     case 1:
+        //         youSelected = `You selected ${selection} out of 5.`;
+        //         break;
+        //     case 2:
+        //         youSelected = `You selected ${selection} out of 5.`;
+        //         break;
+        //     case 3:
+        //         youSelected = `You selected ${selection} out of 5.`;
+        //         break;
+        //     case 4:
+        //         youSelected = `You selected ${selection} out of 5.`;
+        //         break;
+        //     case 5:
+        //         youSelected = `You selected ${selection} out of 5.`;
+        //         break;
+        //     default:
+        //         youSelected = "Please select a rating.";
+        // } 
 
+        console.log(selection)
+    }
 
-/*
-let ratings = {
-    _rating: NaN,
-    set rating(value){
-        if (typeof value === 'number'){
-            this._rating = value;
-        } 
+    ratingSelection();
+
+    let displayEverything = () => {
+
     }
 }
-*/
+
+appRating();
