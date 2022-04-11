273deead635d4df3c3ee4233e2464217875b0dba
Marcus L <marcu99lee@gmail.com>
2022-04-10 21:31:17 -0400

    settings-sync

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const appRating = () => {
     let allBtns = document.querySelectorAll(".rating");
     let lastBtn;
 
-    allBtns.forEach(option => {
+    allBtns.forEach((option) => {
       option.addEventListener("click", function () {
         lastBtn && (lastBtn.style.backgroundColor = null);
         (lastBtn = option).style.backgroundColor = "hsl(25,97%,53%)";
@@ -17,34 +17,32 @@ const appRating = () => {
   btnSeleciton();
 
   function submitButton(selection) {
-    let byeStar = document.queryCommandValue('.star-container');
-    let byeHeader = document.querySelector('.header');
-    let byeCall = document.querySelector('.call');
-    let byeRating = document.querySelector('.button-flex')
+    let byeStar = document.queryCommandValue(".star");
+    let byeHeader = document.querySelector(".header");
+    let byeCall = document.querySelector(".call");
+    let byeRating = document.querySelector(".button-flex");
     let byeContent = [byeStar, byeHeader, byeCall, byeRating];
 
-    byeContent.forEach(option => {
-        console.log(option)
-    })
+    byeContent.forEach((option) => {
+      console.log(option);
+    });
 
-    let showEnd = document.querySelector('.thank-you-state');    
+    let showEnd = document.querySelector(".thank-you-state");
     let answer = document.querySelector(".you-selected").innerText;
     let submitAnswer = document.querySelector(".submit-button");
 
-    
-
-    submitAnswer.addEventListener('click', function () {
-        if (selection) {
-            byeContent.forEach(selection => {
-            selection.style.visibility = 'hidden';
-            showEnd.style.visibility = 'visible';
-            answer = `You selected ${selection} out of 5`;
-            });
-        } else {
-            window.alert('Please help us by selecting a rating.')
-        }
-    })
-  } 
+    submitAnswer.addEventListener("click", function () {
+      if (selection) {
+        byeContent.forEach((selection) => {
+          selection.style.display = "none";
+          showEnd.style.display = "flex";
+          answer = `You selected ${selection} out of 5`;
+        });
+      } else {
+        window.alert("Please help us by selecting a rating.");
+      }
+    });
+  }
 };
 
 appRating();
